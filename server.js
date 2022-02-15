bc8e9044929108f4c2121b47ac56e4d268a83ffb
manumitsacademy <manumits.info@gmail.com>
2022-02-15 13:21:54 +0530

    port log

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app =express();
-var port = process.env.port || 4500
+var port = process.env.port || 4500;
+console.log(port)
 app.use(express.static(__dirname+"/dist/edupolyfrontend/"))
 app.listen(port,()=>{console.log("server running on"+port)})
