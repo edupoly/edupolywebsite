b0024d6125c5456ea84abea48ef506933c142d72
manumitsacademy <manumits.info@gmail.com>
2022-02-16 18:53:44 +0530

    just only comment

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var app =express();
 var port = process.env.PORT || 4500;
 console.log(port)
 app.use(express.static(__dirname+"/dist/edupolyfrontend/"))
-app.listen(port,()=>{console.log("server running on"+port)})
+app.listen(port,()=>{console.log("server running on port:"+port)})
